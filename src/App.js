4cd8c58125ef35bbbc53039f128fe0a15c2abad0
hezzahir <hezzahir>
2023-06-10 00:50:19 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Hassan test MAX5</div>
+        <div>Hassan test MAX6</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
