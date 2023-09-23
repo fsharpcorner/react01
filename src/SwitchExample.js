decc7509f3b97d94da2cad6bec57764425f83bda
hezzahir <hezzahir>
2023-09-23 17:48:21 +0100

    test the switch component

---
+++ src/SwitchExample.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Switch from "react-switch";
+
+const SwitchExample = () => {
+  const [checked, setChecked] = useState(false);
+  const handleChange = nextChecked => {
+    setChecked(nextChecked);
+  };
+
+  return (
+    <div className="example">
+      <h2>Simple usage</h2>
+      <label>
+        <span>Switch with default style</span>
+        <Switch
+          onChange={handleChange}
+          checked={checked}
+          className="react-switch"
+        />
+      </label>
+      <p>
+        The switch is <span>{checked ? "on" : "off"}</span>.
+      </p>
+    </div>
+  );
+};
+
+export default SwitchExample;
